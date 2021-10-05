e6e6b191395b33d72d4cae34d8dd9555ce4ccd6b
unknown <dnguyen@gamingcampus.local>
2021-10-05 17:14:46 +0200

    DepotZone

---
+++ Source/Projet2/DepotZone.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DepotZone.h"
+
+#include "MyGameState.h"
+
+// Sets default values
+ADepotZone::ADepotZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Collider = CreateDefaultSubobject<UBoxComponent>(FName("Collider"));
+	Collider->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void ADepotZone::BeginPlay()
+{
+	Super::BeginPlay();
+	Collider->OnComponentBeginOverlap.AddDynamic(this, &ADepotZone::OnBeginOverlap);
+}
+
+void ADepotZone::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(OtherActor->IsA(AItem::StaticClass()))
+	{
+		GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Red,FString("Overlap Burger!"));
+		AMyGameState* const MyGameState = GetWorld() != NULL ? GetWorld()->GetGameState<AMyGameState>() : NULL;
+		MyGameState->AddScore(1);
+		OtherActor->Destroy();
+	}
+}
+
+// Called every frame
+void ADepotZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Projet2/MyGameState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "MyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET2_API AMyGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public :
+	int Score;
+
+	void AddScore(int);
+};
