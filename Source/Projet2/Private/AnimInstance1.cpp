f4572c3781bb614a50103f087efa220eb3fb1704
janaky bellerophon <bellerophon.janaky@gmail.com>
2021-10-08 00:47:13 +0200

    Début d'animations Player Janaky

---
+++ Source/Projet2/Private/AnimInstance1.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimInstance1.h"
+
+UAnimInstance1::UAnimInstance1()
+{
+	Speed = 0.0;
+}
+
+void UAnimInstance1::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	AActor* OwningActor = GetOwningActor();
+
+	if(OwningActor != nullptr)
+	{
+		Speed = OwningActor->GetVelocity().Size();
+	}
+}
+
+
